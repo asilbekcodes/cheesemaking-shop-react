1700c19c0e46072b420bdd3ad1179de63b8458e3
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-27 09:27:48 +0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
