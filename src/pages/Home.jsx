9dca1b14dbff6e84b6f2611174f81ea79fb3012d
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-28 09:29:00 +0500

    home page add

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../components/Main'
+import MyCarousel from '../components/MyCarousel'
+
+const Home = () => {
+  return (
+    <div>
+      <Main/>
+      <MyCarousel/>
+    </div>
+  )
+}
+
+export default Home
