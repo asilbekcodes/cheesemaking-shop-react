7cc2adac7282173d3a91edbebacf0090834c7361
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-07-30 23:57:51 +0500

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
-import './index.css';
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Card from './pages/Card';
 
 function App() {
+  const [cart, setCart] = useState([])
+  function addToCart({ item}) {
+    setCart ([...cart, item])
+  }
+
+  
   return (
     <React.Fragment>
-      <Header />
+      <Header length={cart.length}/>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/card' element={<Card />} />
+          <Route path='/' element={<Home addToCart={addToCart}/>} />
+          <Route path='/card' element={<Card cart={cart}/>} />
         </Routes>
       <Footer/>
     </React.Fragment>
